e3a4268e1611174a9773dd7514371d2eae3c0eb5
Shiyao <biansyao@gmail.com>
2021-01-11 23:26:47 +0800

    d

--- LinearProgram/LinearProgram.cpp
+++ LinearProgram/LinearProgram.cpp
@@ -1,9 +1,12 @@
-// LinearProgram.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include"read.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Welcome to KoalaOptimizer!\n";
+
+    string fileName = "D:/benchmark/markshare_4_0.mps";
+    Read input(fileName);
+    input.read();
+    return 0;
 }
