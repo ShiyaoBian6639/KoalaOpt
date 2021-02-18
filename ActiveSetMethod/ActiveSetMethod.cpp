f4da91052aede79c2661a162aab0cbd9530d2af1
Shiyao <biansyao@gmail.com>
2021-02-18 23:57:49 +0800

    addition of the active set method

---
+++ ActiveSetMethod/ActiveSetMethod.cpp
@@ -0,0 +1,10 @@
+// ActiveSetMethod.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to Active set method\n";
+}
+ 
